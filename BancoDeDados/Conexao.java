c10faa634c5bef3529bedf9edf792dd140546c32
GabrielbrandaoRP <gabrielhand15@gmail.com>
2024-06-05 18:40:28 -0300

    add Conexao file

---
+++ BancoDeDados/Conexao.java
@@ -0,0 +1,21 @@
+package BancoDeDados;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Conexao {
+    public static Connection GeraConexao() {
+        Connection conexao = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/lavanderia";
+            String usuario = "root";
+            String senha = "F@ce2727";
+            conexao = DriverManager.getConnection(url, usuario, senha);
+            System.out.println("Conexão realizada com sucesso.");
+        } catch (SQLException e) {
+            System.out.println("Erro ao tentar conectar: " + e.getMessage());
+            conexao = null;
+        }
+        return conexao;
+    }
+}
