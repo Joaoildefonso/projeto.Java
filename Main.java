ded8aafd31cf6fd7dd30b3bcc1444c9bf4dc248b
Joaoildefonso <144944014+Joaoildefonso@users.noreply.github.com>
2024-06-06 20:40:34 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,32 +1,52 @@
-import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        Pessoa p = new Pessoa();
-        int servico = 2;
-
-        switch (servico){
-            case 1:
-                System.out.println("Cadastrar ");
-                
-
-                break;
-            case 2:
-                System.out.println("Efetuar login");
-                break;
-            case 3:
-                System.out.println("Escolher serviço");
-                break;
-
-            default:
-                System.out.println("Opção inválida!");
-                break;
+        int servico = 30;
 
 
-
-
+        Connection conexao = null;
+        try {
+            conexao = GeraConexao();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+
+
+        if (conexao != null) {
+            switch (servico) {
+                case 1:
+                    System.out.println("Cadastrar ");
+
+                    break;
+                case 2:
+                    System.out.println("Efetuar login");
+
+                    break;
+                case 3:
+                    System.out.println("Escolher serviço");
+
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        } else {
+            System.out.println("Não foi possível estabelecer uma conexão com o banco de dados.");
+        }
+    }
+
+    public static Connection GeraConexao() throws SQLException {
+        Connection conexao = null;
+
+        String url = "jdbc:mysql://localhost:3306/lavanderia"; // ou "jdbc:mysql://localhost:3306/lavanderia1";
+        String usuario = "root";
+        String senha = "F@ce2727";
+        conexao = DriverManager.getConnection(url, usuario, senha);
+        System.out.println("Conexão realizada com sucesso.");
+        return conexao;
     }
 }
